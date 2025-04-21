180204588b228a669d1fefc8635acbbbff31bb24
Rodrigo Alarcón Milla <hamster22.ok@gmail.com>
2025-04-20 22:22:10 -0400

    Cambios finales
    Se añadió más información de los assets, mecánica principal e inspiraciones. También se añadió las conexiones entre los niveles.

--- Shattered Vision/Assets/MainMenu.cs
+++ Shattered Vision/Assets/MainMenu.cs
@@ -5,16 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-	public void PlayGame()
+	public void PlayTutorial()
 	{
 		SceneManager.LoadSceneAsync(1);
 	}
-	
-	public void QuitGame()
+
+    public void PlayLevelOne()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void QuitGame()
 	{
 		Application.Quit();
 	}
-
 
 	public void EndGame()
 	{
